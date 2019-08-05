af719307de8f6e9bc78a79c63b0087b93692d0d3
kan01234 <safghjkl@gmail.com>
2019-08-05 14:56:00 +0800

    listen TaskDTO

--- src/main/java/com/dotterbear/task/scheduler/consumer/kafka/consumer/TaskConsumer.java
+++ src/main/java/com/dotterbear/task/scheduler/consumer/kafka/consumer/TaskConsumer.java
@@ -4,15 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import com.dotterbear.task.scheduler.consumer.cassandra.entity.Task;
+import com.dotterbear.task.scheduler.consumer.dto.TaskDTO;
 
 @Component
 public class TaskConsumer {
+
   private static final Logger logger = LoggerFactory.getLogger(TaskConsumer.class);
 
   @KafkaListener(topics = {"scheduler-task"}, groupId = "add")
-  public void listen(Task task) {
-    logger.debug("grp1 receive task: {}", task);
+  public void listen(TaskDTO taskDto) {
+    logger.debug("grp1 receive task: {}", taskDto);
   }
 
 }
