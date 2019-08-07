71753ce56103876f0cdf8d29a7e6152873c7fead
kan01234 <safghjkl@gmail.com>
2019-08-07 14:39:29 +0800

    init commit

---
+++ src/main/java/com/dotterbear/task/scheduler/consumer/scheduling/SchedulingService.java
@@ -0,0 +1,12 @@
+package com.dotterbear.task.scheduler.consumer.scheduling;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulingService {
+
+  private static Logger logger =  LoggerFactory.getLogger(SchedulingService.class);
+
+}
